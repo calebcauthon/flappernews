4fa3274eeda12db7e140560bb868b5eabda71c45
Caleb Cauthon <calebcauthon@gmail.com>
2015-10-21 23:26:50 -0500

    Add upvotes.

--- routes/index.js
+++ routes/index.js
@@ -44,6 +44,14 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if(err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
@@ -58,7 +66,9 @@ router.param('post', function(req, res, next, id) {
 
 
 router.get('/posts/:post', function(req, res, next) {
-  res.json(req.post);  
+  req.post.populate('comments', function(erro, post) {
+    res.json(post);
+  });
 });
 
 
